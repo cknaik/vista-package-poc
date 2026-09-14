68c748f8c6b36484f9831b51f3307edfbbc96706
Chintan Naik <chintan.naik@gmail.com>
2026-09-14 22:08:50 +0000

    Remove stale duplicate .m routines

--- routines/XYZBAR.m
+++
@@ -1,9 +0,0 @@
-XYZBAR ; POC - sample fake VistA routine
- ;;1.0;XYZ POC PACKAGE;;2026
- ;
- ; Second fake routine, exercises calling another routine in the package.
- ;
-EN ;
- DO EN^XYZFOO
- DO GREET^XYZFOO("Developer")
- QUIT
